7bb9d6668d86263f17dddc1915fccff5712fd511
XvesS <54544914+XvesS@users.noreply.github.com>
2025-11-24 18:24:18 +0800

    Update command and base path for protein extraction

--- Code/extract_protein.py
+++ Code/extract_protein.py
@@ -1,9 +1,9 @@
 import os
 import torch
 from tqdm import tqdm
-cmd = 'python ./extract.py esm2_t33_650M_UR50D ../Data/Feature/seq_str.fasta ../Data/Feature/esm2_t33_650M_UR50D/ --repr_layers 33 --include per_tok'
+cmd = 'python ./extract.py esm1v_t33_650M_UR90S_1 ../Data/Feature/seq_str.fasta ../Data/Feature/esm1v_t33_650M_UR90S_1/ --repr_layers 33 --include per_tok'
 os.system(cmd)
-base = "../Data/Feature/esm2_t33_650M_UR50D/"
+base = "../Data/Feature/esm1v_t33_650M_UR90S_1/"
 def change(index, layer):
     data = torch.load(base+f'{index}.pt')
     data = data['representations'][layer]
